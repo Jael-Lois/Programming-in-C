32d67937685e0db0924ccf279865f59a319227b5
Grace Hephzibah M <63595093+Grace-Hephzibah@users.noreply.github.com>
2021-05-13 14:54:56 +0530

    Create Arithmetic_Operations.c

---
+++ Switch-Case Statements/Arithmetic_Operations.c
@@ -0,0 +1,48 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // Write C code here
+    int a, b, ans;
+    char sym;
+    
+    printf("Enter 2 numbers : ");
+    scanf("%d %d", &a, &b);
+    
+    printf("\nEnter the arithmetic operator : ");
+    scanf("\n %c", &sym);
+    
+    switch(sym)
+    {
+        case '+':
+            ans = a+b;
+            printf("\nThe answer is %d", ans);
+            break;
+            
+        case '-':
+            ans = a-b;
+            printf("\nThe answer is %d", ans);
+            break;
+            
+        case '*':
+            ans = a*b;
+            printf("\nThe answer is %d", ans);
+            break;
+            
+        case '/':
+            ans = a/b;
+            printf("\nThe answer is %d", ans);
+            break;
+            
+        case '%':
+            ans = a%b;
+            printf("\nThe answer is %d", ans);
+            break;
+            
+        default:
+            printf("\n Enter the correct symbol");
+    }
+    
+    return 0;
+}
